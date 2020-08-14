0988daf67b5efb74c6cb66016f2a750092e0007d
Paul Joachim <63604194+PaulDJoachim@users.noreply.github.com>
2020-08-14 03:25:39 -0500

    made member page lists collapsible

--- src/components/MemberPage/MemberPage.js
+++ src/components/MemberPage/MemberPage.js
@@ -6,6 +6,9 @@ import BookmarkButton from '../BookmarkButton/BookmarkButton'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 
 const listStyle = {
   height: '275px',
@@ -23,7 +26,38 @@ const placeholder = {
   borderRadius: '10px',
 }
 
+
+const styles = theme => ({
+  root: {
+    height: 180,
+  },
+  container: {
+    display: 'flex',
+  },
+  paper: {
+    margin: theme.spacing.unit,
+  },
+  svg: {
+    width: 100,
+    height: 100,
+  },
+  polygon: {
+    fill: theme.palette.common.white,
+    stroke: theme.palette.divider,
+    strokeWidth: 1,
+  },
+});
+
 class MemberPage extends Component {
+
+  state = {
+    collapseVote: false,
+    collapseBill: false,
+    collapseStatement: false,
+    collapseCommittee: false,
+  }
+
+
   componentDidMount() {
     this.props.dispatch({type:'CLEAR_MEMBER'});
     this.props.dispatch({type:'CLEAR_BIO'});
@@ -58,7 +92,25 @@ class MemberPage extends Component {
     this.props.history.push('/Bills/' + id)
   }
 
+  handleStatementCollapse = () => {
+    this.setState(state => ({ collapseStatement: !state.collapseStatement }));
+  };
+
+  handleBillCollapse = () => {
+    this.setState(state => ({ collapseBill: !state.collapseBill }));
+  };
+
+  handleVoteCollapse = () => {
+    this.setState(state => ({ collapseVote: !state.collapseVote }));
+  };
+
+  handleCommitteeCollapse = () => {
+    this.setState(state => ({ collapseCommittee: !state.collapseCommittee }));
+  };
+
   render() {
+
+    const { classes } = this.props;
     let chamber = '2';
     const member = this.props.member;
     if (member.roles === undefined) return null;
@@ -67,58 +119,74 @@ class MemberPage extends Component {
         <div style={placeholder}>
           <img style={listStyle} src={`https://theunitedstates.io/images/congress/225x275/${member.id}.jpg`} />
         </div>
-        <h1>{member.first_name} {member.last_name} - {member.roles[0].party}</h1>
-        <h4>{this.props.bio.extract}</h4>
-        <h2>Website: <a href={member.url}>{member.url}</a></h2>
-        <h2>Phone: {member.roles[0].phone}</h2>
-        <h2>Office: {member.roles[0].office}</h2>
-        <h2>Recent Statements:</h2>
-        <List>
-          {this.props.statements.map((statement, index) => (
-            <a style={{ textDecoration: 'none' }} href={statement.url}>
-              <ListItem button key={index + 's'}>
-                <ListItemText key={index + 's'}>
-                  {statement.date} <br />
-                  {statement.title}
-                </ListItemText>
-              </ListItem>
-            </a>
-          ))}
+        <Typography variant="h3">{member.first_name} {member.last_name} - {member.roles[0].party}</Typography>
+        <Typography variant="body1">{this.props.bio.extract}</Typography> <br />
+        <Typography variant="h6">Website: <a href={member.url}>{member.url}</a></Typography>
+        <Typography variant="h6">Phone: {member.roles[0].phone}</Typography>
+        <Typography variant="h6">Office: {member.roles[0].office}</Typography><br />
+        <Typography variant="h5" onClick={this.handleStatementCollapse}>Recent Statements:</Typography>
+        <List className={classes.root}>
+          <div className={classes.container}>
+            <Collapse in={this.state.collapseStatement}>
+              {this.props.statements.map((statement, index) => (
+                <a style={{ textDecoration: 'none' }} href={statement.url}>
+                  <ListItem button key={index + 's'}>
+                    <ListItemText key={index + 's'}>
+                      {statement.date} <br />
+                      {statement.title}
+                    </ListItemText>
+                  </ListItem>
+                </a>
+              ))}
+            </Collapse>
+          </div>
         </List>
-        <h2>Recent Bills Introduced:</h2>
-        <List>
-          {this.props.bills.map((bill, index) => (
-              <ListItem button key={index + 'b'} onClick={()=>this.handleBillClick(bill.bill_id.slice(0, -4))}>
-                <ListItemText key={index + 'b'}>
-                  {bill.introduced_date} <br />
-                  {bill.number} <br />
-                  {bill.title}
-                </ListItemText>
-              </ListItem>
-          ))}
+        <Typography variant="h5" onClick={this.handleBillCollapse}>Recent Bills Introduced:</Typography>
+        <List className={classes.root}>
+          <div className={classes.container}>
+            <Collapse in={this.state.collapseBill}>
+              {this.props.bills.map((bill, index) => (
+                <ListItem button key={index + 'b'} onClick={()=>this.handleBillClick(bill.bill_id.slice(0, -4))}>
+                  <ListItemText key={index + 'b'}>
+                    {bill.introduced_date} <br />
+                    {bill.number} <br />
+                    {bill.title}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </Collapse>
+          </div>
         </List>
-        <h2>Recent Voting History:</h2>
-        <List>
-          {this.props.votes.map((vote, index) => (
-              <ListItem button key={index + 'v'} onClick={()=>this.handleVoteClick(vote.chamber, vote.session, vote.roll_call)}>
-                <ListItemText key={index + 'v'}>
-                  {vote.date} <br />
-                  Roll Call: {vote.roll_call} <br />
-                  {vote.description} <br />
-                  {vote.position}
-                </ListItemText>
-              </ListItem>
-          ))}
+        <Typography variant="h5" onClick={this.handleVoteCollapse}>Recent Voting History:</Typography>
+        <List className={classes.root}>
+          <div className={classes.container}>
+            <Collapse in={this.state.collapseVote}>
+              {this.props.votes.map((vote, index) => (
+                <ListItem button key={index + 'v'} onClick={()=>this.handleVoteClick(vote.chamber, vote.session, vote.roll_call)}>
+                  <ListItemText key={index + 'v'}>
+                    {vote.date} <br />
+                    Roll Call: {vote.roll_call} <br />
+                    {vote.description} <br />
+                    {vote.position}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </Collapse>
+          </div>
         </List>
-        <h2>Committes</h2>
-        <List>
-          {this.props.member.roles[0].committees.map((committee, index) => (
-            <ListItem button key={index} onClick={()=>this.handleCommitteeClick(committee.chamber, committee.code)}>
-              <ListItemText key={index}>
-                {committee.name}
-              </ListItemText>
-            </ListItem>
-          ))}
+        <Typography variant="h5" onClick={this.handleCommitteeCollapse}>Committes</Typography>
+        <List className={classes.root}>
+          <div className={classes.container}>
+            <Collapse in={this.state.collapseCommittee}>
+              {this.props.member.roles[0].committees.map((committee, index) => (
+                <ListItem button key={index} onClick={()=>this.handleCommitteeClick(committee.chamber, committee.code)}>
+                  <ListItemText key={index}>
+                    {committee.name}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </Collapse>
+          </div>
         </List>
         {/* {JSON.stringify(this.props.member.roles[0].committees)} */}
         <BookmarkButton />
@@ -136,6 +204,7 @@ const mapStateToProps = state => ({
   statements: state.statements.memberStatements,
   bills: state.bills.memberBills,
   votes: state.votes.memberVotes,
+  classes: PropTypes.object.isRequired,
 });
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import 'fontsource-roboto';
 
 import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
